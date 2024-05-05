f3a1744ab3202c2938040267b7a9742a2adf82c1
Rogelio Barajas <brogelio680@gmail.com>
2024-05-04 20:45:12 -0700

    phaser testing

---
+++ public/js/gameJS.js
@@ -0,0 +1,25 @@
+import Phaser from "phaser"
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('test', 'images/LoginBackground.jpg');
+}
+
+function create() {
+    this.add.image(400, 300, "test");
+}
+
+function update() {
+}
